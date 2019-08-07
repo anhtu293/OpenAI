ac1c7e501e6ab48cb089091e7d004b53e4271ca1
Anh Tu Nguyen <anhtu293@gmail.com>
2019-08-07 06:36:55 +0200

    epsilon decay

--- taxi/MC.py
+++ taxi/MC.py
@@ -6,21 +6,22 @@ import sys
 import itertools
 from gym.envs.toy_text.taxi import TaxiEnv
 
-def epsilon_greedy_policy(Q, epsilon, nA):
-	def policy_fn(observation):
-		A = np.ones(nA, dtype = float) * epsilon / nA
-		best_action = np.argmax(Q[observation])
-		A[best_action] += 1 - epsilon
-		return A
-	return policy_fn
+def nth_root(num, n):
+	return(n**(1/num))
 
-def mc_control_epsilon_greedy(env, num_episodes, discount_factor = 1.0, epsilon = 0.1):
+def epsilon_greedy_policy(Q, epsilon, nA, state):
+	A = np.ones(nA, dtype = float) * epsilon / nA
+	best_action = np.argmax(Q[state])
+	A[best_action] += 1 - epsilon
+	return A
+
+def mc_control_epsilon_greedy(env, num_episodes, discount_factor = 1.0, epsilon = 1):
 	returns_sum = defaultdict(float)
 	returns_count = defaultdict(float)
+	final_epsilon = 0.01
+	epsilon_decay = nth_root(num_episodes, final_epsilon/epsilon)
 
 	Q = defaultdict(lambda : np.zeros(env.action_space.n))
-
-	policy = epsilon_greedy_policy(Q, epsilon, env.nA)
 
 	print("\r Start Training")
 	time.sleep(1)
@@ -28,18 +29,23 @@ def mc_control_epsilon_greedy(env, num_episodes, discount_factor = 1.0, epsilon 
 		if (i_episode + 1) % 100 == 0:
 			print("\r Episode {}/{}".format(i_episode + 1, num_episodes), end = "")
 			sys.stdout.flush()
+
+		#Decay epsilon
+		epsilon = epsilon * epsilon_decay
+		#create episode
 		episode = []
 		state = env.reset()
-		action_probs = policy(state)
+		#policy = epsilon_geedy_policy(Q, epsilon, nA, state)
+		action_probs = epsilon_greedy_policy(Q, epsilon, env.action_space.n, state)
 		action = np.random.choice(range(len(action_probs)), p = action_probs)
 		next_state, reward, done, _ = env.step(action)
-		episode.append((next_state, action, reward, done))
+		episode.append((next_state, action, reward))
 		state = next_state
 		while not done:
-			action_probs = policy(state)
+			action_probs = epsilon_greedy_policy(Q, epsilon, env.action_space.n, state)
 			action = np.random.choice(range(len(action_probs)), p = action_probs)
 			next_state, reward, done, _ = env.step(action)
-			episode.append((state, action, reward, done))
+			episode.append((state, action, reward))
 			state = next_state
 
 		state_action_set = set([(x[0], x[1]) for x in episode])
@@ -54,11 +60,15 @@ def mc_control_epsilon_greedy(env, num_episodes, discount_factor = 1.0, epsilon 
 
 def test_policy(env, Q, num_episodes):
 	success = 0
+	print("\n Start Test")
 	for i_episode in range(num_episodes):
 		state = env.reset()
+		print("\r Episode {}/{}".format(i_episode, num_episodes), end = "\n")
 		for t in itertools.count():
 			action = np.argmax(Q[state])
 			next_state, reward, done, _ = env.step(action)
+			#env.render()
+			#time.sleep(0.3)
 			if done:
 				if reward > 0:
 					success += 1
@@ -68,7 +78,7 @@ def test_policy(env, Q, num_episodes):
 
 
 env = gym.make("Taxi-v2")
-train_num_episodes = 200000
+train_num_episodes = 300000
 test_num_episodes = 2000
 
 Q = mc_control_epsilon_greedy(env, train_num_episodes)
